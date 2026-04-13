c55d933e605ae44fa318336219e88b94dc832951
LudSkop <luda.skopenko80@gmail.com>
2026-04-13 20:59:32 +0300

    make NoteForm

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,30 @@
 import css from "./App.module.css";
 import { NoteList } from "../NoteList/NoteList";
 import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
-import { fetchNotes, deleteNote } from "../../services/noteService";
+import { fetchNotes, deleteNote, createNote } from "../../services/noteService";
 import { keepPreviousData } from "@tanstack/react-query";
 import Pagination from "../Pagination/Pagination";
 import { useState } from "react";
 import Modal from "../Modal/Modal";
+import NoteForm from "../NoteForm/NoteForm";
 
 export default function App() {
   const [page, setPage] = useState(1);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const queryClient = useQueryClient();
+
   const { mutate: handleDelete } = useMutation({
     mutationFn: deleteNote, // викликає deleteNote(id)
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["notes"] }); // оновлює список
+    },
+  });
+  // Створення нотатки
+  const { mutate: handleCreateNote } = useMutation({
+    mutationFn: createNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+      setIsModalOpen(false);
     },
   });
 
@@ -45,7 +55,10 @@ export default function App() {
           </button>
           {isModalOpen && (
             <Modal onClose={() => setIsModalOpen(false)}>
-              {/* сюди потім додаси NoteForm */}
+              <NoteForm
+                onClose={() => setIsModalOpen(false)}
+                onSubmit={handleCreateNote}
+              />
             </Modal>
           )}
 
